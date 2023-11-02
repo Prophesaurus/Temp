821d54e81d2ae5dbff8fef298840e6b5104071cd
Prophesaurus <martin.johnen@funkwerk.com>
2023-11-02 12:33:36 +0100

    2. commit

--- src/app/sensor/sensor.component.ts
+++ src/app/sensor/sensor.component.ts
@@ -9,7 +9,12 @@ import { Router } from '@angular/router';
 })
 export class SensorComponent implements OnInit {
   sensorArray:number[]=[];
-  
+  random : number = 0;
+  tempMax : number = 0;
+  Serverschrank : String = "";
+  Adresse : String = "";
+  Hersteller : String = "";
+  avg : number = 0;
   constructor(
     private activatedRoute: ActivatedRoute,
     private http: HttpClient,
@@ -18,31 +23,67 @@ export class SensorComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
-      if(params['id'])
-      this.loadSensor(params['id'].toString())
-    else
-    console.log("Fehler");
+      if(params['id']){
+        this.loadSensor(params['id'].toString())
+
+      }
+      else
+      console.log("Fehler");
     });
   }
 
   loadSensor(ID:String){
     switch (ID){
       case "1":
-        this.sensorArray = [1,2,3,4,5,6,7,8,9,10];
+        this.random = 10;
+        this.tempMax = 15;
+        this.Serverschrank = ID
+        this.Adresse = "Teststraße"+ID;
+        this.Hersteller = "Mustermann";
         break;
       case "2":
-        this.sensorArray = [11,12,13,14,15,16,17,18,19,20];
+        this.random = 20;
+        this.tempMax = 25;
+        this.Serverschrank = ID;
+        this.Adresse = "Teststraße"+ID;
+        this.Hersteller = "Herbert Hersteller";
         break;
       case "3":
-        this.sensorArray = [21,22,23,24,25,26,27,28,29,30];
+        this.random = 30;
+        this.tempMax = 35;
+        this.Serverschrank = ID;
+        this.Adresse = "Teststraße "+ID;
+        this.Hersteller = "Stefan Müller";
         break;
       default:
         console.log("Fehlerhafte ID!");
         break;
     }
+    this.fillSensor();
+    
+  }
+
+  fillSensor(){
+    this.avg = 0;
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.round((this.random + Math.random() * 10 * 2 - 10)*100)/100;
+      this.sensorArray.push(randomNumber);
+      this.avg +=randomNumber;
+    }
+    this.avg = Math.round(this.avg / 10 * 100)/100;
   }
 
   backToMain(){
     this.router.navigate(['main']);
+  }
+
+  changeMaxTemp(){
+    this.tempMax = parseInt((document.getElementById("textInput") as HTMLInputElement).value);
+    console.log(this.tempMax)
+    console.log(sessionStorage.getItem('username'));
+  }
+
+  logOut(){
+    this.router.navigate(['login']);
   }
 }
